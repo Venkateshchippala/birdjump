863d8122abb975a5d9460a37e0a2e13c2538f07b
venky8 <venkateshchippala8@gmail.com>
2025-11-19 19:27:44 +0530

    removed unused code

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -25,16 +25,4 @@ public class GameController : MonoBehaviour
         levelCtrl = allLevels[lvl];
         levelCtrl.gameObject.SetActive(true);
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-       
-    }
-    
 }
